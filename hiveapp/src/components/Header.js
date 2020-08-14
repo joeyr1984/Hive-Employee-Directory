d248e18ab080d81e539a8c00a2032518be1f7bee
Joey Reyes <joeyr1984@gmail.com>
2020-08-14 16:21:30 -0700

    updates to header and input

--- hiveapp/src/components/Header.js
+++ hiveapp/src/components/Header.js
@@ -4,10 +4,40 @@ import "../styles/Header.css";
 
 const styles = {
   headerStyle: {
-    background: "white"
+    background: "white",
+    height: 300
   },
-  headingStyle: {
-    fontSize: 100
+  inputBox: {
+    display: "flex",
+    textAlign: "center",
+    justifyContent: "center"
+  },
+  inputStyle: {
+    height: 40,
+    width: 550,
+    padding: 15,
+    borderRadius: 60
+
+  },
+  buttonStyle: {
+    // position: "absolute",
+    // top: 0,
+    // bottom: 0,
+    // right: 0,
+    color: "white",
+    backgroundColor: "#2e3131",
+    borderRadius: 60,
+    height: 40,
+    width: 100,
+    fontSize: 18,
+    cursor: "pointer"
+  },
+  h1: {
+    fontFamily: "Helvetica",
+    fontSize: 20,
+    fontWeight: "bold",
+    letterSpacing: 30,
+    textAlign: "center"
   }
 };
 
@@ -15,8 +45,14 @@ const styles = {
 
 function Header() {
   return (
-    <header style={styles.headerStyle} className="header">
-      <h1 style={styles.headingStyle}>Welcome</h1>
+    <header style={styles.headerStyle} className="header mt-2">
+      <div style={styles.inputBox} class="input">
+        <input style={styles.inputStyle} type="text" class="form-control mt-5" placeholder="First Name" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+        <div class="input-group-append">
+          <button style={styles.buttonStyle} class="btn btn-outline-secondary mt-5" type="button" id="button-addon2">Search</button>
+        </div>
+      </div>
+      <h1 style={styles.h1} class="mt-5">DIRECTORY</h1>
     </header>
   );
 }
